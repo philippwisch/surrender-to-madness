da958fa25ede5282d76559e2d083e2ba9e7cabe2
Philipp Wischnewski <phil-wisch@web.de>
2023-09-21 22:43:17 +0200

    Added Volume Sliders and Audio Buses

---
+++ volume_slider.gd
@@ -0,0 +1,15 @@
+extends HSlider
+
+
+@export var audio_bus_name = "Master"
+
+@onready var _bus = AudioServer.get_bus_index(audio_bus_name)
+
+
+func _ready():
+	value_changed.connect(_on_value_changed)
+	value = db_to_linear(AudioServer.get_bus_volume_db(_bus))
+
+
+func _on_value_changed(value: float):
+	AudioServer.set_bus_volume_db(_bus, linear_to_db(value))
